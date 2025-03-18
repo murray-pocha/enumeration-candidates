f48a66b3891d17c769912a27ba334fe566c19638
murray-pocha <murray.pocha@gmail.com>
2025-03-18 14:58:09 -0600

    build tasks requied for assignment

--- filters.rb
+++ filters.rb
@@ -2,15 +2,43 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+    @candidates.find { |candidate| candidate[:id] == id }
   end
   
   def experienced?(candidate)
-    # Your code Here
+    candidate[:years_of_experience] >= 2
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    candidates.select do |candidate|
+      experienced?(candidate) &&
+      has_enough_github_points?(candidate) &&
+      knows_ruby_or_python?(candidate) &&
+      applied_recently?(candidate) &&
+      old_enough?(candidate)
+    end
   end
+
   
   # More methods will go below
+
+  def has_enough_github_points?(candidate)
+    candidate[:github_points] >= 100
+  end
+
+  def knows_ruby_or_python?(candidate)
+    candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+  end
+
+  def applied_recently?(candidate)
+    candidate[:date_applied] >= 15.days.ago.to_date
+  end
+
+  def old_enough?(candidate)
+    candidate[:age]  >= 18
+  end
+
+
+def ordered_by_qualifications(candidates)
+  candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+end
--- main.rb
+++ main.rb
@@ -7,3 +7,20 @@ require './filters'
 ## Your test code can go here
 
 pp @candidates
+
+
+@candidates.each do |candidate|
+  puts "Candidate ID #{candidate[:id]} is experienced? #{experienced?(candidate)}"
+end
+
+puts "Finding candidate with ID 5:"
+pp find(5)
+
+puts "\nFinding candidate with ID 15:"
+pp find(15)
+
+puts "Qualified Candidates:"
+pp qualified_candidates(@candidates)
+
+puts "Candidates sorted by experience and GitHub points:"
+pp ordered_by_qualifications(@candidates)
